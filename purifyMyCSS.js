c552f15181e3db5eddaa6ed7d479e133a7bf7098
Lars Jönsson <crymeat@gmail.com>
2020-10-13 16:19:18 +0200

    styling

---
+++ purifyMyCSS.js
@@ -0,0 +1,13 @@
+const purify = require("purify-css")
+
+let content = ['**/views/*.ejs'];
+let css = ['**/public/css/bulma.min.css'];
+let options = {
+  minify: false,
+  whitelist: ['*navbar*', 'image', 'is-64x64', 'is-square', 'is-hidden-touch', 'media-left', 'has-text-weight-normal'],
+	info: true,
+	output: "public/css/output.css"
+}
+purify(content, css, options, (purifiedAndMinifiedResult) => {
+	console.log(purifiedAndMinifiedResult);
+})
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = require('./routes/index.js');
-
-
 const middleware = require('./middleware/index.js');
 
 app.set('view engine', 'ejs');
